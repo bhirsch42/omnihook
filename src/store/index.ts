9dd371b0206fc56fb06cd2a9aa81381de5faf70c
Ben Hirsch <benhirsch42@gmail.com>
2023-03-05 11:54:34 -0500

    add localstorage sync

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Middleware } from "@reduxjs/toolkit";
 import { pilotsReducer } from "./pilots";
+
+const LOCAL_STORAGE_KEY = "omnihook-data";
+
+const localStorageJson = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+const persistedState = localStorageJson ? JSON.parse(localStorageJson) : {};
+
+const localStorageMiddleware: Middleware = ({ getState }) => {
+  return (next) => (action) => {
+    const returnValue = next(action);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(getState()));
+    return returnValue;
+  };
+};
 
 export const store = configureStore({
   reducer: {
     pilots: pilotsReducer,
   },
+  preloadedState: persistedState,
+  middleware: [localStorageMiddleware],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
