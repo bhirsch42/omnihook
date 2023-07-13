4f642b04e3f9451e9e30d4ab53d2ef6d84c1167b
Ben Hirsch <bhirsch@makenotion.com>
2023-07-13 17:32:42 -0400

    https prep

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,12 +4,15 @@
 //! cd examples && cargo run -p example-sqlite
 //! ```
 
-use axum::{response::IntoResponse, routing::get, Extension, Router};
+use std::{net::SocketAddr, sync::Arc};
+
+use axum::{extract::State, response::IntoResponse, routing::get, Extension, Router};
 use axum_login::{
     axum_sessions::{async_session::MemoryStore, SessionLayer},
     secrecy::SecretVec,
     AuthLayer, AuthUser, RequireAuthorizationLayer, SqliteStore,
 };
+use axum_server::tls_rustls::RustlsConfig;
 use rand::Rng;
 use sqlx::sqlite::SqlitePoolOptions;
 
@@ -30,6 +33,31 @@ impl AuthUser<i64> for User {
     }
 }
 
+struct MySQL {
+    pool: sqlx::Pool<sqlx::Sqlite>,
+}
+
+impl MySQL {
+    async fn conn(&self) -> sqlx::pool::PoolConnection<sqlx::Sqlite> {
+        self.pool.acquire().await.unwrap()
+    }
+
+    async fn find_user(&self) -> User {
+        let mut conn = self.conn().await;
+
+        let user: User = sqlx::query_as("select * from users where id = 1")
+            .fetch_one(&mut conn)
+            .await
+            .unwrap();
+
+        user
+    }
+}
+
+struct AppState {
+    mysql: MySQL,
+}
+
 type AuthContext = axum_login::extractors::AuthContext<i64, User, SqliteStore<User>>;
 
 #[tokio::main]
@@ -47,27 +75,14 @@ async fn main() {
     let user_store = SqliteStore::<User>::new(pool);
     let auth_layer = AuthLayer::new(user_store, &secret);
 
-    async fn login_handler(mut auth: AuthContext) {
-        let pool = SqlitePoolOptions::new()
-            .connect("sqlite/user_store.db")
-            .await
-            .unwrap();
-        let mut conn = pool.acquire().await.unwrap();
-        let user: User = sqlx::query_as("select * from users where id = 1")
-            .fetch_one(&mut conn)
-            .await
-            .unwrap();
-        auth.login(&user).await.unwrap();
-    }
-
-    async fn logout_handler(mut auth: AuthContext) {
-        dbg!("Logging out user: {}", &auth.current_user);
-        auth.logout().await;
-    }
-
-    async fn protected_handler(Extension(user): Extension<User>) -> impl IntoResponse {
-        format!("Logged in as: {}", user.name)
-    }
+    let router_state = Arc::new(AppState {
+        mysql: MySQL {
+            pool: SqlitePoolOptions::new()
+                .connect("sqlite/user_store.db")
+                .await
+                .unwrap(),
+        },
+    });
 
     let app = Router::new()
         .route("/protected", get(protected_handler))
@@ -75,10 +90,30 @@ async fn main() {
         .route("/login", get(login_handler))
         .route("/logout", get(logout_handler))
         .layer(auth_layer)
-        .layer(session_layer);
+        .layer(session_layer)
+        .with_state(router_state);
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    println!("Serving on: http://127.0.0.1:3010");
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3010));
+    // let tls_config = RustlsConfig::from_pem_file()
+
+    axum_server::bind(addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn logout_handler(mut auth: AuthContext) {
+    dbg!("Logging out user: {}", &auth.current_user);
+    auth.logout().await;
+}
+
+async fn protected_handler(Extension(user): Extension<User>) -> impl IntoResponse {
+    format!("Logged in as: {}", user.name)
+}
+
+async fn login_handler(State(state): State<Arc<AppState>>, mut auth: AuthContext) {
+    let user = state.mysql.find_user().await;
+    auth.login(&user).await.unwrap();
 }
